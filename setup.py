81302026e09d460195959d719eebb1fc72494317
Lee Kamentsky <lkaments@mit.edu>
2020-12-09 09:29:53 -0500

    Missing comma

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     entry_points={ 'console_scripts': [
         "dcimg2tif=spimstitch.commands.dcimg2tif:main",
         "stack2oblique=spimstitch.commands.stack2oblique:main",
-        "oblique2stitched=spimstitch.commands.stitch_cmd:main"
+        "oblique2stitched=spimstitch.commands.stitch_cmd:main",
         "nonoblique2stitched=spimstitch.commands.stitch_nonoblique_cmd:main"
     ]},
     scripts=["scripts/spimstitch-pipeline.sh"],
